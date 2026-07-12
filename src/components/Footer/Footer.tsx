c0240fae25652923302fd02eb16d73f45766042e
NeoNet <neonetagency@gmail.com>
2026-07-13 02:56:39 +0530

    Designed footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import './Footer.css';
+// import './Footer.css';
+import './footer-test.css';
+import DarkVeil from '../../ui/background/dark-veil';
+import SocialIcons from '../../ui/Social-icons/Social-icons';
 
 function Footer() {
   // This gets the current year automatically so your copyright never expires
@@ -10,16 +13,16 @@ function Footer() {
       <div className="footer-container">
         
         {/* Main Pitch / Call to Action */}
-        <div className="footer-cta">
-          <h2>Ready to secure your digital infrastructure?</h2>
+        {/* <div className="footer-cta"> */}
+          {/* <h2>Ready to secure your digital infrastructure?</h2>
           <p>Let's find out if we are a good fit for each other.</p>
           <a href="mailto:your-email@neonet.com" className="footer-email-btn">
             Get in touch directly
-          </a>
-        </div>
+          </a> */}
+        {/* </div> */}
 
         {/* Bottom Metadata Line */}
-        <div className="footer-bottom">
+        {/* <div className="footer-bottom">
           <div className="footer-brand">
             <img src="/neonet-icon.svg" alt="NeoNet Logo" className="footer-logo" />
             <strong>NeoNet</strong>
@@ -29,6 +32,38 @@ function Footer() {
           <div className="footer-copyright">
             &copy; {currentYear} NeoNet. All rights reserved.
           </div>
+        </div> */}
+
+
+
+       
+
+        <div className='background'>
+        <DarkVeil
+          hueShift={1}
+          noiseIntensity={0}
+          scanlineIntensity={0}
+          speed={1}
+          scanlineFrequency={100}
+          warpAmount={5}
+        />
+        </div>
+        
+         
+        <div className="footer-buttons">
+          <div className='footer-contact'>
+            <h1>Contact us</h1>
+            <p>info@neonet.website</p>
+          </div>
+          <div className = 'footer-socials'>
+            <SocialIcons />
+          </div>
+        </div>
+
+        <div className='widelogo'> 
+          {/* <img src="/neonet-icon.svg" alt="NeoNet Logo" className="footer-logo" /> */}
+          {/* <img src="/neonet-wordmark.svg" alt="NeoNet Wordmark" className="footer-wordmark" /> */}
+          <img src="/footer-logo.svg" alt="NeoNet Logo" className="footer-logo" />
         </div>
 
       </div>
